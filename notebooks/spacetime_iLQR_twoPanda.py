0bbf1b79264ec898223d458c12b79186b6ea8fa8
mnobar <mahdi.nobar@idiap.ch>
2021-06-17 16:23:26 +0200

    demo tailor 4

--- notebooks/spacetime_iLQR_twoPanda.py
+++ notebooks/spacetime_iLQR_twoPanda.py
@@ -53,7 +53,7 @@ for i in range(p.getNumJoints(robot1_id)):
 # getLinkState
 
 # Construct the robot system
-n_iter = 15
+n_iter = 10
 T = 20 # number of data points
 dt = 0.5
 dof = 7
@@ -98,8 +98,8 @@ sys.vis_traj(xs_interp)
 Q_q1=1e-3
 Q_q2=1e-3
 Q = np.diag(np.concatenate((Q_q1*np.ones(7),Q_q2*np.ones(7),[0, 0])))
-QT_s1=1e0
-QT_s2=1e0
+QT_s1=1e-1
+QT_s2=1e-1
 Qf = np.diag(np.concatenate((np.zeros(14),[QT_s1, QT_s2])))
 
 W = np.zeros((6,6))
@@ -112,8 +112,8 @@ Wvia_p2=0
 Wvia = np.diag(np.concatenate((WT_p1*np.ones(3),WT_p2*np.ones(3))))
 
 Rfactor_dq1=1e0
-Rfactor_dq2=2e0
-Rfactor_dq2_j6=2e0
+Rfactor_dq2=1e0
+Rfactor_dq2_j6=1e0
 
 Rfactor_ds1=1e0
 Rfactor_ds2=1e0
@@ -121,7 +121,7 @@ R = np.diag(np.concatenate((Rfactor_dq1*np.array([1,1,1,1,1,1,1]),Rfactor_dq2**n
 
 qobs=1e3
 obs_thresh=2.
-model_Q_obs_s=1e1 # 100 is at the order corrosponding hyper-ellipsoid size 0.1 m
+model_Q_obs_s=1e2 # 100 is at the order corrosponding hyper-ellipsoid size 0.1 m
 # model_Q_obs_x=1e0
 # Qobs=np.diag(np.concatenate((model_Q_obs_x*np.ones(3),[model_Q_obs_s])))
 
@@ -185,9 +185,9 @@ ilqr_cost.set_state(xs, us)  # set initial trajectory
 ilqr_cost.mu = 1e-5
 
 # #### Solve and Plot
-ilqr_cost.solve(n_iter, method='batch', threshold_alpha=1e-5)
+ilqr_cost.solve(n_iter, method='batch', threshold_alpha=1e-6)
 xs_batch, us_batch = ilqr_cost.xs, ilqr_cost.us
-# clear_output()
+clear_output()
 
 # #### Play traj
 # interpolate the virtual time for visualization of both
@@ -202,6 +202,28 @@ for i in range(dof):
 # # unocmment to record only final traj
 p.disconnect()
 physicsClient = p.connect(p.GUI, options="--width=1920 --height=1080 --mp4=\"/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/test.mp4\" --mp4fps=10")  # pybullet with visualisation and recording
+p.resetDebugVisualizerCamera(cameraDistance=2, cameraYaw=30, cameraPitch=-30, cameraTargetPosition=[0,0.5,0])
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.resetSimulation()
+p.loadURDF('plane.urdf')
+robot_urdf = "../data/urdf/frankaemika_new/panda_arm.urdf"
+robot1_id = p.loadURDF(robot_urdf, basePosition=robot1_base_pose, useFixedBase=1)
+robot2_id = p.loadURDF(robot_urdf, basePosition=robot2_base_pose, useFixedBase=1)
+# Create a ball to show the target
+_, _, ballId1 = create_primitives(radius=0.05, rgbaColor=[1, 0, 0, 1])
+_, _, ballId2 = create_primitives(radius=0.05, rgbaColor=[0, 0, 1, 1])
+p.resetBasePositionAndOrientation(ballId1, p_target_1, (0, 0, 0, 1))
+p.resetBasePositionAndOrientation(ballId2, p_target_2, (0, 0, 0, 1))
+
+_, _, ballId1_middle = create_primitives(radius=0.05, rgbaColor=[1, 0, 0, 1])
+p.resetBasePositionAndOrientation(ballId1_middle, ViaPnts1[0], (0, 0, 0, 1))
+
+sys.vis_traj(ilqr_cost.xs, vis_dt=0.1)
+
+clear_output()
+p.disconnect()
+physicsClient = p.connect(p.GUI, options="--width=1920 --height=1080 --mp4=\"/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/test_interp.mp4\" --mp4fps=10")  # pybullet with visualisation and recording
 p.resetDebugVisualizerCamera(cameraDistance=2, cameraYaw=30, cameraPitch=-30, cameraTargetPosition=[0,0.5,0])
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
