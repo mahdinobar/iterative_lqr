d6071478bf134ee285ee1b8e29f8f951ab495d10
mnobar <mahdi.nobar@idiap.ch>
2021-07-22 15:44:40 +0200

    correction of initial configs

--- notebooks/spacetime_iLQR_twoPanda.py
+++ notebooks/spacetime_iLQR_twoPanda.py
@@ -38,9 +38,9 @@ r = R.from_matrix([[np.cos(theta), -np.sin(theta), 0],
 baseOrientation=r.as_quat()
 robot1_id = p.loadURDF(robot_urdf, basePosition=robot1_base_pose, useFixedBase=1)
 robot2_id = p.loadURDF(robot_urdf, basePosition=robot2_base_pose, baseOrientation=baseOrientation, useFixedBase=1)
-p_target_1 = np.array([.7, 0.0, .5])
-p_target_2 = np.array([.7, 0.2, .5])
-ViaPnts1=np.array([[.4, .6, .5]])
+p_target_1 = np.array([.7, .0, .5])
+p_target_2 = np.array([.7, .2, .5])
+ViaPnts1=np.array([[.5, .5, .5]])
 ViaPnts2=np.array([])
 joint_limits = get_joint_limits(robot1_id, 7)
 
@@ -59,34 +59,34 @@ for i in range(p.getNumJoints(robot1_id)):
 # getLinkState
 
 # Construct the robot system
-n_iter = 10
+n_iter = 25
 T = 40 # number of data points
 dt = 0.5
 dof = 7
 sys = URDFRobot_spacetime_dual(dof=dof, robot1_id=robot1_id, robot2_id=robot2_id, dt=dt)
 
-# # Set the initial state
-# # comment for warm start
-# # q0_1 = np.array([0., 0., 0., 0., 0., 0., 0.])
-# # q0_2 = np.array([0., 0., 0., 0., 0., 0., 0.])
-# q0_1 = np.array([0.09628129916964914, 0.2069041213575445, -0.3148163748471766, -0.28248525520291473, 0.04714093813300132, 0.5138600262006123, 0.162604552546608])
-# q0_2 = np.array([0.09628129916964914, 0.2069041213575445, -0.3148163748471766, -0.28248525520291473, 0.04714093813300132, 0.5138600262006123, 0.162604552546608])
-# x0 = np.concatenate([q0_1, q0_1, np.zeros(2)])
+# Set the initial state
+# comment for warm start
+# q0_1 = np.array([0., 0., 0., 0., 0., 0., 0.])
+# q0_2 = np.array([0., 0., 0., 0., 0., 0., 0.])
+q0_1=np.mean(joint_limits,0)
+q0_2=np.mean(joint_limits,0)
+x0 = np.concatenate([q0_1, q0_1, np.zeros(2)])
 
-# uncomment to warm start traj
-us=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor_nonZero.npy")
-xs=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor_nonZero.npy")
-x0=xs[0,:]
+# # uncomment to warm start traj
+# us=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor.npy")
+# xs=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor.npy")
+# x0=xs[0,:]
 
 sys.set_init_state(x0)
 
-# ### Set initial control output
-# # set initial control output to be all zeros
-# # add epsilon offset to avoid barrier
-# # # comment for warm start
-# us = np.hstack((np.zeros((T + 1, sys.Du-2)),1e-3*np.ones((T + 1, 2))))
-# _ = sys.compute_matrices(x=None, u=us[0])
-# xs = sys.rollout(us[:-1])
+### Set initial control output
+# set initial control output to be all zeros
+# add epsilon offset to avoid barrier
+# # comment for warm start
+us = np.hstack((np.zeros((T + 1, sys.Du-2)),1e-3*np.ones((T + 1, 2))))
+_ = sys.compute_matrices(x=None, u=us[0])
+xs = sys.rollout(us[:-1])
 
 # #### Try forward kinematics
 pos1_0, quat1_0, pos2_0, quat2_0 = sys.compute_ee(x0, link_id)
@@ -127,7 +127,7 @@ Rfactor_ds1=1e-10
 Rfactor_ds2=1e-10
 R = np.diag(np.concatenate((Rfactor_dq1*np.array([1,1,1,1,1,1,1]),Rfactor_dq2**np.array([1,1,1,1,1]),Rfactor_dq2_j6**np.array([1]),Rfactor_dq2**np.array([1]),[Rfactor_ds1,Rfactor_ds2])))
 
-qobs=1e0
+qobs=0
 obs_thresh=2
 model_Q_obs_s=2
 # model_Q_obs_x=1e0
@@ -192,25 +192,25 @@ ilqr_cost.set_state(xs, us)  # set initial trajectory
 # todo for batch?
 # ilqr_cost.mu = 1e-5
 
-# # #### Solve and Plot
+# #### Solve and Plot
 ilqr_cost.solve(n_iter, method='batch', threshold_alpha=1e-5)
-# xs_batch, us_batch = ilqr_cost.xs, ilqr_cost.us
-# clear_output()
-#
-# # #### Play traj
-# # interpolate the virtual time for visualization of both
-# nbVis=50
-# xs_interp=np.zeros((nbVis, ilqr_cost.xs.shape[1]))
-# tt=np.linspace(0,np.max(ilqr_cost.xs[:,14:]), nbVis)
-# for i in range(dof):
-#     xs_interp[:,i] = np.interp(tt, ilqr_cost.xs[:,14],ilqr_cost.xs[:,i])
-#     xs_interp[:,dof+i] = np.interp(tt, ilqr_cost.xs[:, 15], ilqr_cost.xs[:, dof+i])
+xs_batch, us_batch = ilqr_cost.xs, ilqr_cost.us
+clear_output()
 
-xs_interp=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs_interp.npy")
+# #### Play traj
+# interpolate the virtual time for visualization of both
+nbVis=50
+xs_interp=np.zeros((nbVis, ilqr_cost.xs.shape[1]))
+tt=np.linspace(0,np.max(ilqr_cost.xs[:,14:]), nbVis)
+for i in range(dof):
+    xs_interp[:,i] = np.interp(tt, ilqr_cost.xs[:,14],ilqr_cost.xs[:,i])
+    xs_interp[:,dof+i] = np.interp(tt, ilqr_cost.xs[:, 15], ilqr_cost.xs[:, dof+i])
+
+# xs_interp=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs_interp.npy")
 # # unocmment to record only final traj
 p.disconnect()
 physicsClient = p.connect(p.GUI, options="--width=1920 --height=1080 --mp4=\"/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/test.mp4\" --mp4fps=10")  # pybullet with visualisation and recording
-p.resetDebugVisualizerCamera(cameraDistance=2, cameraYaw=30, cameraPitch=-30, cameraTargetPosition=[0,0.5,0])
+p.resetDebugVisualizerCamera(cameraDistance=2, cameraYaw=30, cameraPitch=-90, cameraTargetPosition=[0,0.5,0])
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.resetSimulation()
@@ -228,7 +228,7 @@ _, _, ballId1_middle = create_primitives(radius=0.05, rgbaColor=[1, 0, 0, 1])
 p.resetBasePositionAndOrientation(ballId1_middle, ViaPnts1[0], (0, 0, 0, 1))
 
 sys.vis_traj(xs_interp, vis_dt=0.1)
-# np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs_interp.npy",xs_interp)
+np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs_interp.npy",xs_interp)
 # for i in range(21):
 #     print(i)
 #     sys.compute_ee(ilqr_cost.xs[i,:], link_id)
@@ -238,6 +238,6 @@ pos1, _, pos2, _ = sys.compute_ee(ilqr_cost.xs[-1], link_id)
 
 print('pos1-p_target_1={}, pos2-p_target_2={}'.format(pos1-p_target_1, pos2-p_target_2))
 
-# # # uncomment to save warm start traj
-# np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor_nonZero.npy",ilqr_cost.xs)
-# np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor_nonZero.npy",ilqr_cost.us)
+# # uncomment to save warm start traj
+np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor.npy",ilqr_cost.xs)
+np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor.npy",ilqr_cost.us)
