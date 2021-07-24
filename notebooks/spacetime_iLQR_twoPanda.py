6a84bbe500edca9c13f058a45a5e1de527eb559f
mnobar <mahdi.nobar@idiap.ch>
2021-07-24 21:58:34 +0200

    dev [#2]

--- notebooks/spacetime_iLQR_twoPanda.py
+++ notebooks/spacetime_iLQR_twoPanda.py
@@ -40,7 +40,7 @@ robot1_id = p.loadURDF(robot_urdf, basePosition=robot1_base_pose, useFixedBase=1
 robot2_id = p.loadURDF(robot_urdf, basePosition=robot2_base_pose, baseOrientation=baseOrientation, useFixedBase=1)
 p_target_1 = np.array([.5, .2, .4])
 p_target_2 = np.array([.5, .4, .4])
-ViaPnts1=np.array([[.3, .5, .4]])
+ViaPnts1=np.array([[.3, .4, .6]])
 ViaPnts2=np.array([])
 joint_limits = get_joint_limits(robot1_id, 7)
 
@@ -65,28 +65,28 @@ dt = 0.5
 dof = 7
 sys = URDFRobot_spacetime_dual(dof=dof, robot1_id=robot1_id, robot2_id=robot2_id, dt=dt)
 
-# # Set the initial state
-# # comment for warm start
-# # q0_1 = np.array([0., 0., 0., 0., 0., 0., 0.])
-# # q0_2 = np.array([0., 0., 0., 0., 0., 0., 0.])
-# q0_1=np.mean(joint_limits,0)
-# q0_2=np.mean(joint_limits,0)
-# x0 = np.concatenate([q0_1, q0_1, np.zeros(2)])
+# Set the initial state
+# comment for warm start
+# q0_1 = np.array([0., 0., 0., 0., 0., 0., 0.])
+# q0_2 = np.array([0., 0., 0., 0., 0., 0., 0.])
+q0_1=np.mean(joint_limits,0)
+q0_2=np.mean(joint_limits,0)
+x0 = np.concatenate([q0_1, q0_1, np.zeros(2)])
 
-# uncomment to warm start traj
-us=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor.npy")
-xs=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor.npy")
-x0=xs[0,:]
+# # uncomment to warm start traj
+# us=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor.npy")
+# xs=np.load("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor.npy")
+# x0=xs[0,:]
 
 sys.set_init_state(x0)
 
-# ### Set initial control output
-# # set initial control output to be all zeros
-# # add epsilon offset to avoid barrier
-# # # comment for warm start
-# us = np.hstack((np.zeros((T + 1, sys.Du-2)),1e-3*np.ones((T + 1, 2))))
-# _ = sys.compute_matrices(x=None, u=us[0])
-# xs = sys.rollout(us[:-1])
+### Set initial control output
+# set initial control output to be all zeros
+# add epsilon offset to avoid barrier
+# # comment for warm start
+us = np.hstack((np.zeros((T + 1, sys.Du-2)),1e-3*np.ones((T + 1, 2))))
+_ = sys.compute_matrices(x=None, u=us[0])
+xs = sys.rollout(us[:-1])
 
 # #### Try forward kinematics
 pos1_0, quat1_0, pos2_0, quat2_0 = sys.compute_ee(x0, link_id)
@@ -111,25 +111,25 @@ QT_s2=1e0
 Qf = np.diag(np.concatenate((np.zeros(14),[QT_s1, QT_s2])))
 
 W = np.zeros((6,6))
-WT_p1=1e4
-WT_p2=1e4
+WT_p1=1e3
+WT_p2=1e3
 WT = np.diag(np.concatenate((WT_p1*np.ones(3),WT_p2*np.ones(3))))
 
-Wvia_p1=1e4
+Wvia_p1=1e3
 Wvia_p2=0
 Wvia = np.diag(np.concatenate((WT_p1*np.ones(3),WT_p2*np.ones(3))))
 
-Rfactor_dq1=1e1
+Rfactor_dq1=1e0
 Rfactor_dq2=1e0
 Rfactor_dq2_j2=1e0
 
-Rfactor_ds1=1e0
-Rfactor_ds2=1e0
+Rfactor_ds1=1e-10
+Rfactor_ds2=1e-10
 R = np.diag(np.concatenate((Rfactor_dq1*np.array([1,1,1,1,1,1,1]),Rfactor_dq2**np.array([1]),Rfactor_dq2_j2**np.array([1]),Rfactor_dq2**np.array([1,1,1,1,1]),[Rfactor_ds1,Rfactor_ds2])))
 
-qobs=1e3
-obs_thresh=1
-model_Q_obs_s=1
+qobs=0
+obs_thresh=2
+model_Q_obs_s=2
 # model_Q_obs_x=1e0
 # Qobs=np.diag(np.concatenate((model_Q_obs_x*np.ones(3),[model_Q_obs_s])))
 
@@ -239,6 +239,6 @@ pos1, _, pos2, _ = sys.compute_ee(ilqr_cost.xs[-1], link_id)
 
 print('pos1-p_target_1={}, pos2-p_target_2={}'.format(pos1-p_target_1, pos2-p_target_2))
 
-# # # uncomment to save warm start traj
-# np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor.npy",ilqr_cost.xs)
-# np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor.npy",ilqr_cost.us)
+# # uncomment to save warm start traj
+np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/xs0_tailor.npy",ilqr_cost.xs)
+np.save("/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/us0_tailor.npy",ilqr_cost.us)
