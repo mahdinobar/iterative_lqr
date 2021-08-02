jacobian base orientation caused rotation corrected

notebooks/ocp_sys.py
@@ -291,10 +291,10 @@ class URDFRobot_spacetime_dual():
         Jl2, Ja2 = p.calculateJacobian(self.robot2_id, ee_id, [0., 0., 0.], x[self.dof:self.dof*2].tolist(), zeros, zeros)
         Jl2, Ja2 = np.array(Jl2), np.array(Ja2)
 
-        ### Important
-        # if baseOrientation is modified for robot2_id then the jacobian should be rotated back to be with respect ot World coordinate system
-        Jl2[[0, 1]] = Jl2[[1, 0]]
-        Jl2[[1]] = -Jl2[[1]]
+        # ### todo Important
+        # # if baseOrientation is modified for robot2_id then the jacobian should be rotated back to be with respect ot World coordinate system
+        # Jl2[[0, 1]] = Jl2[[1, 0]]
+        # Jl2[[1]] = -Jl2[[1]]
 
         self.J2 = np.concatenate([Jl2, Ja2], axis=0)
         return self.J1, self.J2

notebooks/spacetime_iLQR_twoPanda.py
@@ -14,8 +14,8 @@ import pybullet_data
 np.set_printoptions(precision=4, suppress=True)
 # #### Setup pybullet with the urdf
 # configure pybullet and load plane.urdf and quadcopter.urdf
-physicsClient = p.connect(p.DIRECT)  # pybullet only for computations no visualisation, faster
-# physicsClient = p.connect(p.GUI)  # pybullet with visualisation
+# physicsClient = p.connect(p.DIRECT)  # pybullet only for computations no visualisation, faster
+physicsClient = p.connect(p.GUI)  # pybullet with visualisation
 # physicsClient = p.connect(p.GUI, options="--width=1920 --height=1080 --mp4=\"/home/mahdi/RLI/codes/iterative_lqr/notebooks/tmp/NIST/test.mp4\" --mp4fps=10")  # pybullet with visualisation and recording
 # p.resetDebugVisualizerCamera(cameraDistance=2, cameraYaw=30, cameraPitch=-90, cameraTargetPosition=[0,0.,0])
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)